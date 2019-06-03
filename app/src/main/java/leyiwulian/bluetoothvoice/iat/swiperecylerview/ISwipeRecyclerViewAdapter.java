780430d039f57a33e836c0bc083bf695d55b98ec
sinde12 <sinde12@qq.com>
2019-06-03 10:42:43 +0800

    Update ISwipeRecyclerViewAdapter.java

--- app/src/main/java/leyiwulian/bluetoothvoice/iat/swiperecylerview/ISwipeRecyclerViewAdapter.java
+++ app/src/main/java/leyiwulian/bluetoothvoice/iat/swiperecylerview/ISwipeRecyclerViewAdapter.java
@@ -2,7 +2,7 @@ package leyiwulian.bluetoothvoice.iat.swiperecylerview;
 
 public interface ISwipeRecyclerViewAdapter {
 
-    /**
+       /**
      * @return left function width dp
      * **/
     int leftFunctionWidth(int position);
@@ -11,5 +11,17 @@ public interface ISwipeRecyclerViewAdapter {
      * @return right function width dp
      * **/
     int rightFunctionWidth(int position);
+
+    /**
+     * 进入编辑模式
+     * @param editView function view
+     * **/
+    void enterEditMode(int position, View editView);
+
+    /**
+     * 退出编辑模式
+     * @param editView function view
+     * **/
+    void exitEditMode(int position,View editView);
 
 }
